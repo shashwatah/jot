ffff2140ee66376e104a32ab4a51cea22b7d2988
araekiel <shashwatdeadcliff@gmail.com>
2022-07-28 23:00:13 +0530

    update: clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{Parser};
+
+/// Blazingly fast cli note management.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the vault to enter
+    #[clap(short, long, value_parser)]
+    enter_vault: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args)
 }
