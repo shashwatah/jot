d231ed1a54c3024542d666bf97ab84a0b9a2e624
araekiel <shashwatdeadcliff@gmail.com>
2022-07-31 23:06:28 +0530

    update(minor): src/cli.rs; exec: cargo fmt

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,5 @@
+use clap::{AppSettings, Parser, Subcommand, ValueEnum};
 use std::path::PathBuf;
-use clap::{Parser, Subcommand, AppSettings, ValueEnum};
 
 #[derive(Parser, Debug)]
 #[clap(global_setting(AppSettings::DeriveDisplayOrder))]
@@ -19,7 +19,7 @@ enum Command {
         #[clap(value_parser)]
         location: Option<PathBuf>,
         #[clap(subcommand)]
-        command: Option<VltCommand>
+        command: Option<VltCommand>,
     },
     /// operations for notes.
     NTS {
@@ -30,26 +30,28 @@ enum Command {
         #[clap(value_parser)]
         location: Option<PathBuf>,
         #[clap(subcommand)]
-        command: Option<NtsCommand>
+        command: Option<NtsCommand>,
     },
     /// fs operations for directories.
     DIR {
-        /// name for the directory to be created. 
+        /// name for the directory to be created.
         #[clap(value_parser)]
         dir_name: Option<String>,
         #[clap(subcommand)]
-        command: Option<DirCommand>
+        command: Option<DirCommand>,
     },
     /// switch directories with standard fs syntax.
     CDR {
         /// location of the directory to switch to.
         #[clap(value_parser)]
-        location: PathBuf
+        location: PathBuf,
     },
     /// list and open notes from current vault's history.
     HST {
         #[clap(subcommand)]
-        command: Option<HstCommand>
+        command: Option<HstCommand>,
+        // #[clap(short, action)]
+        // open: bool
     },
     /// open last accessed note in the current vault.
     LST,
@@ -59,7 +61,7 @@ enum Command {
         #[clap(value_parser)]
         query: String,
         #[clap(value_enum, value_parser)]
-        search_type: SearchType
+        query_type: QueryType,
     },
     /// list, create and delete memos or quick notes (independent of current vault).
     MEM {
@@ -67,22 +69,21 @@ enum Command {
         #[clap(value_parser)]
         content: Option<String>,
         #[clap(subcommand)]
-        command: Option<MemCommand>
+        command: Option<MemCommand>,
+        // #[clap(short, action)]
+        // del: bool
     },
     /// show this help message or help for given command.
     Help,
 }
 
 #[derive(Subcommand, Debug)]
+#[clap(args_conflicts_with_subcommands = true)]
 enum VltCommand {
     /// enter a vault.
-    ENT {
-        vault_name: String,
-    },
+    ENT { vault_name: String },
     /// delete a vault.
-    DEL {
-        vault_name: String,
-    },
+    DEL { vault_name: String },
     /// rename a vault.
     REN {
         vault_name: String,
@@ -92,64 +93,54 @@ enum VltCommand {
     MOV {
         vault_name: String,
         new_location: PathBuf,
-    }
+    },
 }
 
 #[derive(Subcommand, Debug)]
+#[clap(args_conflicts_with_subcommands = true)]
 enum NtsCommand {
     /// enter a note.
-    OPN {
-        note_name: String,
-    },
+    OPN { note_name: String },
     /// delete a note.
-    DEL {
-        note_name: String,
-    },
+    DEL { note_name: String },
     /// rename a note.
-    REN {
-        note_name: String,
-        new_name: String,
-    },
+    REN { note_name: String, new_name: String },
     /// move note to new location.
     MOV {
         note_name: String,
         new_location: PathBuf,
-    }
+    },
 }
 
-
 #[derive(Subcommand, Debug)]
+#[clap(args_conflicts_with_subcommands = true)]
 enum DirCommand {
     /// delete a directory.
-    DEL {
-        dir_name: String,
-    },
+    DEL { dir_name: String },
     /// rename a directory.
-    REN {
-        dir_name: String,
-        new_name: String,
-    },
+    REN { dir_name: String, new_name: String },
     /// move directory to a new location.
     MOV {
         dir_name: String,
         new_location: PathBuf,
-    }
+    },
 }
 
 #[derive(Subcommand, Debug)]
 enum HstCommand {
     /// open a note from history.
-    OP
+    OPN,
 }
 
 #[derive(ValueEnum, Clone, Debug)]
-enum SearchType {
-    FIL, 
-    DIR
+enum QueryType {
+    FIL,
+    DIR,
 }
 
-#[derive(Subcommand, Debug)] 
+#[derive(Subcommand, Debug)]
+#[clap(args_conflicts_with_subcommands = true)]
 enum MemCommand {
     /// choose which memo to delete.
-    DEL
+    DEL,
 }
