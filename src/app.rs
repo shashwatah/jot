73947fcd64bf9f4fd446023773eb0c585037fb86
araekiel <shashwatdeadcliff@gmail.com>
2022-08-04 21:53:50 +0530

    update: src/app.rs: handle_args

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
-use crate::args::Args;
+use crate::args::{Args, Command, VltCommand};
 use crate::config::Config;
-use crate::vault::Vault;
+use crate::vault::{Vault, self};
 use clap::Parser;
 
 #[allow(dead_code)]
@@ -14,7 +14,7 @@ pub struct App {
 impl App {
     pub fn new() -> Self {
         App {
-            config: Config::load_config().unwrap(),
+            config: Config::load_config(),
             args: Args::parse(),
             current_vault: None,
         }
@@ -26,5 +26,58 @@ impl App {
 
     pub fn display_args(&self) {
         println!("{:#?}", self.args)
+    }
+
+
+    // absoluut spaghet 🤌
+    pub fn handle_args(&mut self) {
+        match &self.args.command {
+            Command::VLT {name, path, command} => {
+                if let Some(VltCommand::ENT { name }) = command {
+                    self.config.update_current_vault(String::from(name));
+                    println!("vaults: {:#?}", self.config.get_vaults());
+                    match self.config.get_current_vault() {
+                        Some(current_vault) => println!("current vault: {}", current_vault),
+                        None => println!("Not inside a vault")
+                    }
+                }
+
+                if let Some(VltCommand::DEL { name }) = command {
+                    if let Some(vault) = self.config.get_current_vault() {
+                        if name == vault {
+                            self.config.exit_current_vault()
+                        }
+                    }
+                    self.config.del_vault(name);
+                    println!("vaults: {:#?}", self.config.get_vaults());
+                    match self.config.get_current_vault() {
+                        Some(current_vault) => println!("current vault: {}", current_vault),
+                        None => println!("Not inside a vault")
+                    }
+                }
+
+                if let Some(name_str) = name {
+                    if let Some(path_str) = path {
+                        self.config.add_vault(String::from(name_str), String::from(path_str));
+                        println!("vaults: {:#?}", self.config.get_vaults())
+                    }
+                }
+
+                if let None = name {
+                    if let None = command {
+                        println!("vaults: {:#?}", self.config.get_vaults().keys());
+                        match self.config.get_current_vault() {
+                            Some(current_vault) => println!("current vault: {}", current_vault),
+                            None => println!("Not inside a vault")
+                        }
+                    }
+                }
+                
+            },
+            _ => {
+                self.display_config();
+                self.display_args()
+            }
+        }
     }
 }
