aba076952a4874925ca78550fabfeafc0063d13c
araekiel <shashwatdeadcliff@gmail.com>
2022-08-04 21:51:59 +0530

    update: src/config.rs: update methods

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
-enum FileFormat {
+pub enum FileFormat {
     Text,
     Markdown,
 }
@@ -13,7 +13,9 @@ enum FileFormat {
 pub struct Config {
     editor: String,
     format: FileFormat,
-    vaults: HashMap<String, String>,
+    current_vault: Option<String>,
+    vaults: HashMap<String, String>
+    
 }
 
 impl Default for Config {
@@ -22,13 +24,53 @@ impl Default for Config {
             editor: "nvim".to_string(),
             format: FileFormat::Markdown,
             vaults: HashMap::new(),
+            current_vault: None 
         }
     }
 }
 
 impl Config {
-    pub fn load_config() -> Result<Self, confy::ConfyError> {
-        let config: Config = confy::load("jot")?;
-        Ok(config)
+    pub fn load_config() -> Self {
+        let config: Config = confy::load("jot").unwrap();
+        config
+    }
+
+    pub fn get_vaults(&self) -> &HashMap<String, String> {
+        &self.vaults
+    }
+
+    pub fn get_current_vault(&self) -> &Option<String> {
+        &self.current_vault
+    }
+
+    pub fn update_editor(&mut self, editor: String) {
+        self.editor = editor;
+        confy::store("jot", self).unwrap()
+    }
+    pub fn update_format(&mut self, format: FileFormat) {
+        self.format = format;
+        confy::store("jot", self).unwrap()
+    }
+
+    pub fn add_vault(&mut self, name: String, path: String) {
+        self.vaults.entry(name).or_insert(path);
+        confy::store("jot", self).unwrap()
+    }
+    
+    pub fn del_vault(&mut self, name: &str) {
+        self.vaults.remove(name);
+        confy::store("jot", self).unwrap()
+    }
+
+    pub fn update_current_vault(&mut self, name: String) {
+        if self.vaults.contains_key(&name) {
+            self.current_vault = Some(name);
+            confy::store("jot", self).unwrap()
+        }
+    }
+
+    pub fn exit_current_vault(&mut self) {
+        self.current_vault = None;
+        confy::store("jot", self).unwrap()
     }
 }
