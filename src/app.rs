2f9281c893f9065b2d08bbc5889ef6a8d8cf8d2b
araekiel <shashwatdeadcliff@gmail.com>
2022-08-10 22:30:07 +0530

    update: src: app.rs, vault.rs: handle_args & del handle_vlt_cmd

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
-use crate::args::{Args, Command};
+use crate::args::{Args, ItemType, SubCommand};
 use crate::config::Config;
-use crate::vault::handle_vlt_command;
+use crate::vault::{create_vault, delete_vault, enter_vault, move_vault, rename_vault};
 use clap::Parser;
 
 #[allow(dead_code)]
@@ -28,14 +28,55 @@ impl App {
 
     // absoluut spaghet 🤌
     pub fn handle_args(&mut self) {
-        match &self.args.command {
-            Command::VLT {
-                name,
-                path,
-                command,
-            } => {
-                handle_vlt_command(name, path, command, &mut self.config);
+        match &self.args.subcommand {
+            SubCommand::VLT { name, location } => {
+                // if name and path are some -> create vault with name and path
+                match name {
+                    Some(name_value) => {
+                        if let Some(path_value) = location {
+                            create_vault(name_value, path_value, &mut self.config);
+                            return;
+                        }
+                    }
+                    None => {
+                        println!("vaults: {:#?}", self.config.get_vaults().keys());
+                        match self.config.get_current_vault() {
+                            Some(current_vault) => println!("current vault: {}", current_vault),
+                            None => println!("not inside a vault"),
+                        }
+                    }
+                }
             }
+            SubCommand::ENT { name } => enter_vault(name, &mut self.config),
+            SubCommand::REN {
+                item_type,
+                name,
+                new_name,
+            } => match item_type {
+                ItemType::VLT => rename_vault(name, new_name, &mut self.config),
+                _ => {
+                    self.display_config();
+                    self.display_args()
+                }
+            },
+            SubCommand::DEL { item_type, name } => match item_type {
+                ItemType::VLT => delete_vault(name, &mut self.config),
+                _ => {
+                    self.display_config();
+                    self.display_args()
+                }
+            },
+            SubCommand::MOV {
+                item_type,
+                name,
+                new_location,
+            } => match item_type {
+                ItemType::VLT => move_vault(name, new_location, &mut self.config),
+                _ => {
+                    self.display_config();
+                    self.display_args()
+                }
+            },
             _ => {
                 self.display_config();
                 self.display_args()
--- src/vault.rs
+++ src/vault.rs
@@ -1,52 +1,11 @@
-use crate::args::VltCommand;
+// use crate::args::VltCommand;
 use crate::config::Config;
 use crate::fs::{
     check_path, create_directory, create_path_string, delete_directory, move_dirctory,
     rename_directory,
 };
 
-pub fn handle_vlt_command(
-    name: &Option<String>,
-    path: &Option<String>,
-    command: &Option<VltCommand>,
-    config: &mut Config,
-) {
-    // if name and path are some -> create vault with name and path
-    if let Some(name_value) = name {
-        if let Some(path_value) = path {
-            create_vault(name_value, path_value, config);
-        }
-    }
-
-    if let Some(VltCommand::ENT { name }) = command {
-        enter_vault(name, config);
-    }
-
-    if let Some(VltCommand::DEL { name }) = command {
-        delete_vault(name, config);
-    }
-
-    if let Some(VltCommand::REN { name, new_name }) = command {
-        rename_vault(name, new_name, config);
-    }
-
-    if let Some(VltCommand::MOV { name, new_path }) = command {
-        move_vault(name, new_path, config);
-    }
-    
-    // no arg passed -> display all vaults, highlight current vault
-    if let None = name {
-        if let None = command {
-            println!("vaults: {:#?}", config.get_vaults().keys());
-            match config.get_current_vault() {
-                Some(current_vault) => println!("current vault: {}", current_vault),
-                None => println!("not inside a vault"),
-            }
-        }
-    }
-}
-
-fn create_vault(name: &str, path: &str, config: &mut Config) {
+pub fn create_vault(name: &str, path: &str, config: &mut Config) {
     // check if vault name doesn't already exist
     if config.check_vault(name) == false {
         // check if path is exists
@@ -68,7 +27,7 @@ fn create_vault(name: &str, path: &str, config: &mut Config) {
     }
 }
 
-fn enter_vault(name: &str, config: &mut Config) {
+pub fn enter_vault(name: &str, config: &mut Config) {
     if config.check_vault(name) == true {
         if let Some(current_vault) = config.get_current_vault() {
             if current_vault == name {
@@ -83,7 +42,7 @@ fn enter_vault(name: &str, config: &mut Config) {
     }
 }
 
-fn rename_vault(name: &str, new_name: &str, config: &mut Config) {
+pub fn rename_vault(name: &str, new_name: &str, config: &mut Config) {
     // check if vault exists
     if config.check_vault(name) == true {
         rename_directory(name, new_name, config.get_vault_path(name).unwrap());
@@ -100,7 +59,7 @@ fn rename_vault(name: &str, new_name: &str, config: &mut Config) {
     }
 }
 
-fn delete_vault(name: &str, config: &mut Config) {
+pub fn delete_vault(name: &str, config: &mut Config) {
     if config.check_vault(name) == true {
         if let Some(vault) = config.get_current_vault() {
             if name == vault {
