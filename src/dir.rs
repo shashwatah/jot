290a7e7e5d0b610c23ccbdb2f9dd10d6e96f233d
araekiel <shashwatdeadcliff@gmail.com>
2022-08-15 21:56:38 +0530

    update: src/dir.rs: change_dir; reafactor: create_dir, print_dir_tree

--- src/dir.rs
+++ src/dir.rs
@@ -1,44 +1,55 @@
-use crate::config::Config;
-use crate::fs::{create_folder, create_path_with_fslash, join_paths};
+use crate::fs::{collapse_path, create_folder, join_paths, path_exists, unix_path};
 use crate::vault::Vault;
 use walkdir::WalkDir;
 
-pub fn create_dir(name: &str, config: &Config, current_vault: &mut Vault) {
-    let current_vault_name = config.get_current_vault().unwrap();
-    let current_vault_path = config.get_vault_locaton(current_vault_name).unwrap();
+pub fn create_dir(name: &str, current_vault: &mut Vault) {
+    let vault_name = current_vault.get_name();
+    let vault_location = current_vault.get_location();
     let current_location = current_vault.get_current_location();
-    let full_path = join_paths(vec![
-        current_vault_path,
-        current_vault_name,
-        current_location,
-        name,
-    ]);
+
+    let full_path = join_paths(vec![vault_location, vault_name, current_location, name]);
+
     create_folder(&full_path);
-    println!(
-        "folder {} created at {}",
-        name,
-        create_path_with_fslash(&full_path)
-    )
+
+    println!("{} created at {}", name, unix_path(&full_path))
 }
 
-pub fn print_dir_tree(config: &Config, current_vault: &Vault) {
-    let current_vault_name = config.get_current_vault().unwrap();
-    let current_vault_location = config.get_vault_locaton(current_vault_name).unwrap();
+pub fn print_dir_tree(current_vault: &Vault) {
+    let vault_name = current_vault.get_name();
+    let vault_location = current_vault.get_location();
     let current_location = current_vault.get_current_location();
-    let full_path = join_paths(vec![
-        current_vault_location,
-        current_vault_name,
-        current_location,
-    ]);
+
+    let full_path = join_paths(vec![vault_location, vault_name, current_location]);
+
     for entry in WalkDir::new(&full_path).into_iter().filter_map(|e| e.ok()) {
-        println!(
-            "{}",
-            create_path_with_fslash(&entry.path().to_str().unwrap().to_string())
-        );
+        println!("{}", unix_path(&entry.path().to_str().unwrap().to_string()));
     }
 }
 
-// pub fn change_dir() {}
+pub fn change_dir(location: &str, current_vault: &mut Vault) {
+    let vault_name = current_vault.get_name();
+    let vault_location = current_vault.get_location();
+    let current_locaton = current_vault.get_current_location();
+
+    let vault_path = join_paths(vec![vault_location, vault_name]);
+    let vault_path = unix_path(&vault_path);
+
+    let full_path = join_paths(vec![&vault_path, current_locaton, location]);
+    let full_path = collapse_path(&full_path);
+    let full_path = unix_path(&full_path);
+
+    if path_exists(&full_path) & full_path.contains(&vault_path) {
+        let mut new_location = full_path.replace(&vault_path, "");
+        if new_location.starts_with("/") {
+            new_location = new_location[1..].to_string();
+        }
+        current_vault.update_current_location(&new_location);
+        println!("changed dir");
+    } else {
+        panic!("invalid path")
+    }
+}
+
 // pub fn rename_dir() {}
 // pub fn delete_dir() {}
 // pub fn move_dir() {}
