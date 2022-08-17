3c0b6ccaa94896b58d738cf54dcb971b8a646e0a
araekiel <shashwatdeadcliff@gmail.com>
2022-08-17 23:36:53 +0530

    fix: src/note.rs: md

--- src/note.rs
+++ src/note.rs
@@ -11,7 +11,8 @@ fn create_note_path(location_data: (&str, &str, &str), name: &str) -> String {
 }
 
 pub fn create_note(name: &str, current_vault: &Vault) {
-    let full_path = create_note_path(current_vault.get_location_data(), name);
+    let name = name.to_string() + ".md";
+    let full_path = create_note_path(current_vault.get_location_data(), &name);
 
     if path_exists(&full_path) == false {
         create_file(&full_path);
@@ -24,23 +25,28 @@ pub fn create_note(name: &str, current_vault: &Vault) {
 pub fn rename_note(name: &str, new_name: &str, current_vault: &Vault) {
     let (vault_name, vault_location, current_location) = current_vault.get_location_data();
 
+    let name = name.to_string() + ".md";
+    let new_name = new_name.to_string() + ".md";
+
     if name == new_name {
         panic!("new name can't be same as old name")
     }
 
     let path = join_paths(vec![vault_location, vault_name, current_location]);
 
-    if path_exists(&join_paths(vec![&path, new_name])) == true {
+    if path_exists(&join_paths(vec![&path, &new_name])) == true {
         panic!("note named {} already exists", new_name)
     }
 
-    rename_item(name, new_name, &path);
+    rename_item(&name, &new_name, &path);
 
     println!("note {} renamed to {}", name, new_name)
 }
 
 pub fn move_note(name: &str, new_location: &str, current_vault: &Vault) {
     let (vault_name, vault_location, current_location) = current_vault.get_location_data();
+
+    let name = name.to_string() + ".md";
 
     let path = join_paths(vec![vault_location, vault_name, current_location]);
     let path = unix_path(&path);
@@ -49,7 +55,7 @@ pub fn move_note(name: &str, new_location: &str, current_vault: &Vault) {
     let new_path = collapse_path(&new_path);
     let new_path = unix_path(&new_path);
 
-    if path_exists(&join_paths(vec![&new_path, name])) {
+    if path_exists(&join_paths(vec![&new_path, &name])) {
         panic!("note named {} already exists at new path", name)
     }
 
@@ -57,11 +63,13 @@ pub fn move_note(name: &str, new_location: &str, current_vault: &Vault) {
         panic!("invalid path")
     }
 
-    move_item(name, &path, &new_path);
+    move_item(&name, &path, &new_path);
     print!("note {} moved", name)
 }
 
 pub fn movev_note(name: &str, new_vault: &str, config: &Config, current_vault: &Vault) {
+    let name = name.to_string() + ".md";
+    
     if let Some(new_vault_location) = config.get_vault_location(new_vault) {
         let (current_vault_name, current_vault_location, current_location) =
             current_vault.get_location_data();
@@ -73,7 +81,7 @@ pub fn movev_note(name: &str, new_vault: &str, config: &Config, current_vault: &
         let new_path = join_paths(vec![new_vault_location, new_vault]);
 
         // this will be bypassed if the user enters a path instead of name for a note
-        if path_exists(&join_paths(vec![&new_path, name])) == true {
+        if path_exists(&join_paths(vec![&new_path, &name])) == true {
             panic!("note named {} already exists in vault {}", name, new_vault)
         }
 
@@ -83,7 +91,7 @@ pub fn movev_note(name: &str, new_vault: &str, config: &Config, current_vault: &
             current_location,
         ]);
 
-        move_item(name, &original_path, &new_path);
+        move_item(&name, &original_path, &new_path);
         print!("note {} moved to {}", name, new_vault)
     } else {
         panic!("vault {} doesn't exist", new_vault)
@@ -91,11 +99,11 @@ pub fn movev_note(name: &str, new_vault: &str, config: &Config, current_vault: &
 }
 
 pub fn delete_note(name: &str, current_vault: &Vault) {
-    let full_path = create_note_path(current_vault.get_location_data(), name);
+    let name = name.to_string() + ".md";
+
+    let full_path = create_note_path(current_vault.get_location_data(), &name);
 
     delete_file(&full_path);
 
     print!("note {} deleted", name)
 }
-
-// pub fn open_note() {}
