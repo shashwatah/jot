b2c29f6451ee9b3be30c1ed6f3da6c1a66c4fefd
araekiel <shashwatdeadcliff@gmail.com>
2022-08-24 00:27:53 +0530

    refactor: src/fs.rs
    removed path_exists; add valid name check; join_paths accepts generic: AsRef<Path>; collapse_path > process_path; paths/locations: String > PathBuf

--- src/fs.rs
+++ src/fs.rs
@@ -2,62 +2,58 @@ use fs_extra::{dir::CopyOptions, move_items};
 use std::fs::{remove_dir_all, remove_file, rename, DirBuilder, File};
 use std::path::{Path, PathBuf};
 
-use path_slash::PathExt as _;
-
-pub fn path_exists(path: &str) -> bool {
-    Path::new(path).exists()
+pub fn valid_name(name: &str) -> bool {
+    name.chars().all(|char| !r#"\/?%*:|"<>"#.contains(char))
 }
 
-pub fn join_paths(paths: Vec<&str>) -> &str {
+pub fn join_paths<T: AsRef<Path>>(paths: Vec<T>) -> PathBuf {
     let mut full_path = PathBuf::new();
     for path in paths {
         full_path.push(path);
     }
-    full_path.to_str().unwrap()
+    full_path
 }
 
-pub fn collapse_path(path: &str) -> String {
-    let mut new_path = PathBuf::new();
-    for element in PathBuf::from(path).iter() {
+// returns new pathbuf -> with slashes formatted according to os & '..'s collapsed
+// use this when storing or displaying paths
+pub fn process_path(path: &PathBuf) -> PathBuf {
+    let mut processed_path = PathBuf::new();
+
+    for element in path.iter() {
         if element == ".." {
-            new_path.pop();
+            processed_path.pop();
         } else if element != "." {
-            new_path.push(element);
+            processed_path.push(element);
         }
     }
-    return new_path.to_str().unwrap().to_string();
+
+    processed_path
 }
 
-pub fn unix_path(path: &str) -> String {
-    // using path_slash crate to convert system created paths on windows (uses \ instead of /)
-    // to unix style paths to maintain consistency when printing paths.
-    Path::new(path).to_slash().unwrap().to_string()
-}
-
-pub fn create_folder(path: &str) {
+pub fn create_folder(path: &PathBuf) {
     DirBuilder::new().create(path).unwrap();
 }
 
-pub fn delete_folder(path: &str) {
+pub fn delete_folder(path: &PathBuf) {
     remove_dir_all(path).unwrap()
 }
 
-pub fn create_file(path: &str) {
+pub fn create_file(path: &PathBuf) {
     File::create(path).unwrap();
 }
 
-pub fn delete_file(path: &str) {
+pub fn delete_file(path: &PathBuf) {
     remove_file(path).unwrap();
 }
 
-pub fn rename_item(name: &str, new_name: &str, path: &str) {
-    let original_path = join_paths(vec![path, name]);
-    let new_path = join_paths(vec![path, new_name]);
+pub fn rename_item(name: &str, new_name: &str, location: &PathBuf) {
+    let original_path = join_paths(vec![location.to_str().unwrap(), name]);
+    let new_path = join_paths(vec![location.to_str().unwrap(), new_name]);
     rename(original_path, new_path).unwrap();
 }
 
-pub fn move_item(name: &str, path: &str, new_path: &str) {
+pub fn move_item(name: &str, original_location: &PathBuf, new_location: &PathBuf) {
     // using crate: *fs_extra* here but i might implement a custom recursive move function later
-    let original_path = vec![join_paths(vec![path, name])];
-    move_items(&original_path, new_path, &CopyOptions::new()).unwrap();
+    let original_path = vec![join_paths(vec![original_location.to_str().unwrap(), name])];
+    move_items(&original_path, new_location, &CopyOptions::new()).unwrap();
 }
