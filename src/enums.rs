61abaffb2b91975881434c3f0b5684f60ef6ce29
araekiel <shashwatdeadcliff@gmail.com>
2022-09-03 20:30:13 +0530

    rename: src: types > enums; update: moved Vault out of types & added ConfigType

---
+++ src/enums.rs
@@ -0,0 +1,54 @@
+use clap::ValueEnum;
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum Item {
+    Vl,
+    Nt,
+    Fd,
+}
+
+impl Item {
+    pub fn to_vault_item(&self) -> VaultItem {
+        match self {
+            Item::Vl => VaultItem::Fd,
+            Item::Nt => VaultItem::Nt,
+            Item::Fd => VaultItem::Fd,
+        }
+    }
+
+    pub fn full(&self) -> &str {
+        match self {
+            Item::Vl => "vault",
+            Item::Nt => "note",
+            Item::Fd => "folder",
+        }
+    }
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum VaultItem {
+    Nt,
+    Fd,
+}
+
+impl VaultItem {
+    pub fn to_item(&self) -> Item {
+        match self {
+            VaultItem::Nt => Item::Nt,
+            VaultItem::Fd => Item::Fd,
+        }
+    }
+
+    pub fn full(&self) -> &str {
+        match self {
+            VaultItem::Nt => "note",
+            VaultItem::Fd => "folder",
+        }
+    }
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum ConfigType {
+    Editor,
+    Conflict,
+}
