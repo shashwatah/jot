2760e219c52675ca3c8a3a354afc4ee3fd17668d
araekiel <shashwatdeadcliff@gmail.com>
2022-09-07 22:26:32 +0530

    update: src: error.rs: added variants

--- src/error.rs
+++ src/error.rs
@@ -1,45 +1,51 @@
-/*
-- Errors:
-    - DataError:
-        - couldn't load config/data
-    - VaultExists
-    - ItemDoesntExist
-        - Folder/Note
-    - ItemAlreadyExists
-    - InvalidName
-    - PathDoesntExist
-    - InvalidPath
-    - SameName
-    - InvalidEditor
-*/
-
-use std::io::ErrorKind;
 use std::fmt::Display;
+use std::io::{Error as OSError, ErrorKind};
 
+// writing boilerplate error code atm, will probably replace this with thiserror in future
+
+#[allow(unused)]
 #[derive(Debug)]
 pub enum Error {
-    AlreadyExists,
-    NotFound,
-    Unspecified
+    IOError {
+        // converted from io::std::Error > namely NotFound & AlreadyExists
+        source: OSError,
+    },
+    FileError,     // errors concering FileIO trait
+    InternalError, // internal erros: unwrap calls that fail, internal err result matches
+    InvalidName,
+    VaultAlreadyExists(String),
+    VaultNotFound(String),
+    NotInsideVault,
+    PathOutOfBounds,
+    EditorNotFound,
+    Misc,
 }
+
+/*
+    errors that are not exactly errors:
+        already in vault {} -- upon en_vault
+        {item} {}  already exists in vault {} -- upon v_move
+        {item} {} already exist in this location -- upon move
+
+*/
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            Error::AlreadyExists => "already exists",
-            Error::NotFound => "not found",
-            Error::Unspecified => "unspecified error",
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                _ => Error::Misc,
+            }
+        )
     }
 }
 
 impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
         match error.kind() {
-            ErrorKind::AlreadyExists => Error::AlreadyExists,
-            ErrorKind::NotFound => Error::NotFound,
-            _ => Error::Unspecified            
+            ErrorKind::NotFound | ErrorKind::AlreadyExists => Error::IOError { source: error },
+            _ => Error::Misc,
         }
     }
 }
-
